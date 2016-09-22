0881067f4e1b597ddbd0efe12727690e2a2f62d5
echoe <sfelshman@gmail.com>
2016-09-22 10:24:53 -0500

    testing this change to auto-rewrite the wp config.

--- remakemysqlusers.sh
+++ remakemysqlusers.sh
@@ -23,23 +23,28 @@ else
   echo "No wpconf available in default location!"; 
   exit 1; 
 fi
+if [[ -f /home/$myuser/$olddbname.wpscript -o -f $wpconf.script ]]; then 
+  echo "Some script location not OK, did you run this script before?"; 
+  exit 1; 
+else 
+  echo "Script file locations OK";
+fi
 #set the rest of the variables we need to UAPI it up
 olddbname=$(grep DB_NAME $wpconf | cut -d"'" -f4)
+olddbusr=$(grep DB_USER $wpconf | cut -d"'" -f4)
 dbname=`echo $mynameuser$dbextension`;
 dbusr=`echo $mynameuser$userextension`;
 #create the user, db, and set privileges
 uapi --user=$myuser Mysql create_database name=$dbname;
 uapi --user=$myuser Mysql create_user name=$dbusr password=$passwd;
 uapi --user=$myuser Mysql set_privileges_on_database user=$dbusr database=$dbname privileges="ALL PRIVILEGES";
-#check for file existing and if it doesn't, dump stuff and then import it
-if [[ -f /home/$myuser/$olddbname.wpscript ]]; then 
-  echo "DB backup location not OK, did you run this script before?"; 
-  exit 1; 
-else 
-  echo "DB Backup location OK";
-fi
 mysqldump $olddbname > /home/$myuser/$olddbname.wpscript
 mysql $dbname < /home/$myuser/$olddbname
 rm -f /home/$myuser/$olddbname
-echo "we are grabbing info from $wpconf for user $myuser and are making this for $dbname and $dbusr with password $passwd"
-echo "Please replace the password/user/etc. with the new values in wpconfig listed above and test. Then, if it works, run this: echo 'drop database $olddbname' | mysql . Thanks\!"
+cp $wpconf $wpconf.wpscript
+sed -i 's/$olddbname/$dbname/g' $wpconf
+sed -i 's/$olddbusr/$dbusr/g' $wpconf
+sed -i "s/^define('DB_PASSWORD.*/define('DB_PASSWORD'), '$passwd');/g" $wpconf
+echo "we grabbed info from $wpconf for user $myuser and made this for $dbname and $dbusr with password $passwd"
+echo "we automatically edited the wp-config file, backup at $wpconf.wpscript"
+echo "Please test. Then, if it works, run this to drop the old database: echo 'drop database $olddbname' | mysql"
